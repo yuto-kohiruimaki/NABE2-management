a897338cb73cc8ddb0904f8523a123348996712b
Chanter327 <march09yuuto@gmail.com>
2023-09-01 02:33:05 +0900

    change app title and add default timezone

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module NABE2Management
     # in config/environments, which are processed later.
     #
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
