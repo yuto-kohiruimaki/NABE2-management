952738eab2197f35bf4306a8f25d216332a34e93
Chanter327 <march09yuuto@gmail.com>
2023-09-01 01:28:32 +0900

    fix log in function

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -1,2 +1,46 @@
 class TimesController < ApplicationController
+    before_action :authenticate_user!
+  
+    def index
+      @timestamps = Timestamp.all.order(updated_at: "DESC")
+    end
+  
+    def new
+      @userName = current_user.name
+    end
+  
+    def create
+      now = Date.today
+      this_year = now.year
+  
+      timestamp = Timestamp.new(name:params[:name], year: this_year, month:params[:month], date:params[:date], start_time_h:params[:start_time_h], start_time_m:params[:start_time_m], finish_time_h:params[:finish_time_h], finish_time_m:params[:finish_time_m], desc:params[:desc], user_id:current_user.id,)
+      timestamp.save
+      redirect_to times_path
+    end
+  
+    def edit
+      @timestamp = Timestamp.find_by(id: params[:id])
+    end
+  
+    def update
+      @timestamp = Timestamp.find_by(id: params[:id])
+      @timestamp.name = params[:name]
+      @timestamp.month = params[:month]
+      @timestamp.date = params[:date]
+      @timestamp.start_time_h = params[:start_time_h]
+      @timestamp.start_time_m = params[:start_time_m]
+      @timestamp.finish_time_h = params[:finish_time_h]
+      @timestamp.finish_time_m = params[:finish_time_m]
+      @timestamp.desc = params[:desc]
+      @timestamp.save
+      redirect_to times_path
+    end
+  
+    def destroy
+      timestamp_post = Timestamp.find_by(id: params[:id])
+      timestamp_post.destroy
+      redirect_to times_path
+    end
+  
 end
+  
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
 import "controllers"
+
+const logo = document.getElementById('logo');
+const hmb = document.getElementById('hmb');
+const menu = document.getElementById('menu');
+
+hmb.addEventListener('click', () => {
+    logo.classList.toggle('active');
+    hmb.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+//= require jquery_ujs
