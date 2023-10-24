5d338e7fbe2ffde2a5003e394fa578c5b694c5e2
Chanter327 <march09yuuto@gmail.com>
2023-10-25 02:03:45 +0900

    update post function and add some js files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 
+// menu
 const logo = document.getElementById('logo');
 const hmb = document.getElementById('hmb');
 const menu = document.getElementById('menu');
@@ -10,30 +11,3 @@ hmb.addEventListener('click', () => {
     hmb.classList.toggle('active');
     menu.classList.toggle('active');
 });
-
-const getDay = new Date();
-const thisMonth = getDay.getMonth() + 1;
-const today = getDay.getDate();
-
-const month = document.getElementById('month');
-const monthOptions = month.options;
-const monthArr = {};
-for (let i = 0; i < monthOptions.length; i ++) {
-        monthArr[i] = monthOptions[i].value;
-}
-const monthArrValues = Object.values(monthArr);
-const findMonth = monthArrValues.find((num) => num == thisMonth);
-monthOptions[findMonth - 1].selected = true;
-
-
-const date = document.getElementById('date');
-const dateOptions = date.options;
-const dateArr ={};
-for (let i = 0; i < dateOptions.length; i ++) {
-    dateArr[i] = dateOptions[i].value;
-}
-const dateArrValues = Object.values(dateArr);
-const findDate = dateArrValues.find((num) => num == today);
-dateOptions[findDate - 1].selected = true;
-
-//= require jquery_ujs
---
+++ app/javascript/timestamp.js
@@ -0,0 +1,23 @@
+const getDay = new Date();
+const thisMonth = getDay.getMonth() + 1;
+const today = getDay.getDate();
+
+const month = document.getElementById('month');
+const monthOptions = month.options;
+const monthArr = {};
+for (let i = 0; i < monthOptions.length; i ++) {
+        monthArr[i] = monthOptions[i].value;
+}
+const monthValuesArr = Object.values(monthArr);
+const findMonth = monthValuesArr.find((num) => num == thisMonth);
+monthOptions[findMonth - 1].selected = true;
+
+const date = document.getElementById('date');
+const dateOptions = date.options;
+const dateArr ={};
+for (let i = 0; i < dateOptions.length; i ++) {
+    dateArr[i] = dateOptions[i].value;
+}
+const dateValuesArr = Object.values(dateArr);
+const findDate = dateValuesArr.find((num) => num == today);
+dateOptions[findDate - 1].selected = true;
